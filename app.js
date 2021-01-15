7320ffb3edcedbf31206d5774caeba8271080515
Terry Park <tereuspark@gmail.com>
2021-01-15 17:13:44 -0600

    variable port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
 	res.send("Welcome to the homepage!");
 });
 
-const port = 5000;
+const port = process.env.port || 5000;
 app.listen(port, () => {
 	console.log(`App is running on ${port}`);
 });
